7ef76de1cc38276f06771c791fe5979baf581de2
Ophelie <freville.ophelie@neuf.fr>
2024-01-11 15:06:08 +0100

    Ajout de fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Corrformation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+        $formation = new Corrformation(); // Corrformation : nom de l'entité 
+
+        $formation->setTitre('Une Formation TrucMuche');
+        $formation->setDescription('Une decription de la formation TrucMuche');
+        $formation->setDuree(3);
+        $formation->setNiveau('Expert');
+        $formation->setLieu('presentiel');
+        $manager->persist($formation);
 
         $manager->flush();
     }
